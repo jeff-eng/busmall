b265bc20d08e2f9f13d347261a338d5958cc7a95
Jeff Eng <jeff@jeffeng.me>
2016-01-20 14:36:17 -0800

    no duplicated images and generated results button once 15 clicks completed

--- app.js
+++ app.js
@@ -1,27 +1,27 @@
-
+"use strict";
 //Object Constructor for creating product objects
 function Product (productName, filePath, clicks, timesDisplayed) {
   this.productName = productName;
   this.filePath = filePath;
-  this.clicks = clicks;
-  this.timesDisplayed = timesDisplayed;
+  this.clicks = 0;
+  this.timesDisplayed = 0;
 }
 
 //Create instances
-var bag = new Product('bag', 'img/bag.jpg', 0, 0);
-var banana = new Product('banana', 'img/banana.jpg', 0, 0);
-var boots = new Product('boots', 'img/boots.jpg', 0, 0);
-var chair = new Product('chair', 'img/chair.jpg', 0, 0);
-var cthulhu = new Product('cthulhu', 'img/cthulhu.jpg', 0, 0);
-var dragon = new Product('dragon', 'img/dragon.jpg', 0, 0);
-var pen = new Product('pen', 'img/pen.jpg', 0, 0);
-var scissors = new Product('scissors', 'img/scissors.jpg', 0, 0);
-var shark = new Product('shark', 'img/shark.jpg', 0, 0);
-var sweep = new Product('sweep', 'img/sweep.png', 0, 0);
-var unicorn = new Product('unicorn', 'img/unicorn.jpg', 0, 0);
-var usb = new Product('usb', 'img/usb.gif', 0, 0);
-var watercan = new Product('water can', 'img/water-can.jpg', 0, 0);
-var wineglass = new Product('wine glass', 'img/wine-glass.jpg', 0, 0);
+var bag = new Product('bag', 'img/bag.jpg');
+var banana = new Product('banana', 'img/banana.jpg');
+var boots = new Product('boots', 'img/boots.jpg');
+var chair = new Product('chair', 'img/chair.jpg');
+var cthulhu = new Product('cthulhu', 'img/cthulhu.jpg');
+var dragon = new Product('dragon', 'img/dragon.jpg');
+var pen = new Product('pen', 'img/pen.jpg');
+var scissors = new Product('scissors', 'img/scissors.jpg');
+var shark = new Product('shark', 'img/shark.jpg');
+var sweep = new Product('sweep', 'img/sweep.png');
+var unicorn = new Product('unicorn', 'img/unicorn.jpg');
+var usb = new Product('usb', 'img/usb.gif');
+var watercan = new Product('water can', 'img/water-can.jpg');
+var wineglass = new Product('wine glass', 'img/wine-glass.jpg');
 
 //Put instances of objects into an array
 var productsList = [bag, banana, boots, chair, cthulhu, dragon, pen, scissors, shark, sweep, unicorn, usb, watercan, wineglass];
@@ -32,51 +32,83 @@ var productCenter = document.getElementById('productCenter');
 var productRight = document.getElementById('productRight');
 
 //Create an event listener that responds to the click on an image, then records the click to the object and then starts a function that randomly selects another image.  Run a loop?
-
-// productLeft.addEventListener('click', handleClickOnProductLeft);
-// productCenter.addEventListener('click', handleClickOnProductCenter);
+productLeft.addEventListener('click', handleClickOnProductLeft);
+productCenter.addEventListener('click', handleClickOnProductCenter);
 productRight.addEventListener('click', handleClickOnProductRight);
 
+productRight.src = productsList[0].filePath;
+
+
 //Declare global variables to store the random numbers generated within the function
-// var globalRandomOne = randomOne;
-// var globalRandomTwo = randomTwo;
-// var globalRandomThree = randomThree;
+var randomNumLeft, randomNumCenter, randomNumRight;
+imageMaker();
+
+var totalClicks = 0;
 
 // Creating a function to display a random image from the productsList array.
-// function handleClickOnProductLeft() {
-//   console.log('Left was clicked');
-//   var randomNumOne = Math.floor(Math.random() * productsList.length);
-//   console.log(randomNumOne);
-//   productLeft.src = productsList[randomNumOne].filePath;
-//   var randomNumTwo = Math.floor(Math.random() * productsList.length);
-//   console.log(randomNumTwo);
-//   productCenter.src = productsList[randomNumTwo].filePath;
-//   var randomNumThree = Math.floor(Math.random() * productsList.length);
-//   console.log(randomNumThree);
-//   productRight.src = productsList[randomNumThree].filePath;
-// }
-
-// function handleClickOnProductCenter() {
-//   console.log('Center was clicked');
-//   var randomNumOne = Math.floor(Math.random() * productsList.length);
-//   console.log(randomNumOne);
-//   productCenter.src = productsList[randomNumOne].filePath;
-//   var randomNumTwo = Math.floor(Math.random() * productsList.length);
-//   console.log(randomNumTwo);
-//   productLeft.src = productsList[randomNumTwo].filePath;
-//   var randomNumThree = Math.floor(Math.random() * productsList.length);
-//   console.log(randomNumThree);
-//   productRight.src = productsList[randomNumThree].filePath;
-// }
-//
-function handleClickOnProductRight() {
-  console.log('Right was clicked');
-  var randomNumOne = Math.floor(Math.random() * productsList.length);
-  console.log(randomNumOne);
-  productRight.src = productsList[randomNumOne].filePath;
-  var randomNumTwo = Math.floor(Math.random() * productsList.length);
-  productLeft.src = productsList[randomNumTwo].filePath
+function handleClickOnProductLeft() {
+  productsList[randomNumLeft].clicks++;
+  console.log('Left was clicked');
+  imageMaker();
+  totalClicks += 1;
+  console.log('The total number of clicks is ' + totalClicks);
+  if (totalClicks === 15) {
+    displayResults();
+  }
 }
 
-//Create a random integer
-// Math.floor(Math.random() * productsList.length)
+function handleClickOnProductCenter() {
+  productsList[randomNumCenter].clicks++;
+  console.log('Center was clicked');
+  imageMaker();
+  totalClicks += 1;
+  console.log('The total number of clicks is ' + totalClicks);
+  if (totalClicks === 15) {
+    displayResults();
+  }
+}
+
+function handleClickOnProductRight() {
+  productsList[randomNumRight].clicks++;
+  console.log('Right was clicked');
+  imageMaker();
+  totalClicks += 1;
+  console.log('The total number of clicks is ' + totalClicks);
+  if (totalClicks === 15) {
+    displayResults();
+  }
+}
+
+//Reusable function to creates a random integer
+function randoMaker() {
+  var randomNumber = Math.floor(Math.random() * productsList.length);
+  return randomNumber;
+}
+
+//Function to create a set of 3 unique images on the screen and log how many times the image displayed
+function imageMaker() {
+  randomNumLeft = randoMaker();
+  console.log(randomNumLeft);
+  randomNumCenter = randoMaker();
+  while(randomNumLeft === randomNumCenter) {
+    randomNumCenter = randoMaker();
+  }
+  console.log(randomNumCenter);
+  randomNumRight = randoMaker();
+  while(randomNumRight === randomNumLeft || randomNumRight === randomNumCenter) {
+    randomNumRight = randoMaker();
+  }
+  console.log(randomNumRight);
+  productLeft.src = productsList[randomNumLeft].filePath;
+  productsList[randomNumLeft].timesDisplayed++;
+  productCenter.src = productsList[randomNumCenter].filePath;
+  productsList[randomNumCenter].timesDisplayed++;
+  productRight.src = productsList[randomNumRight].filePath;
+  productsList[randomNumRight].timesDisplayed++;
+}
+
+//Function that displays results after total number of clicks is reached
+function displayResults() {
+  var button = document.getElementById('button');
+  button.removeAttribute('hidden');
+}
